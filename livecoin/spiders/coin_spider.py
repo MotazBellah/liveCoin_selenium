6ae33236d0c0dd77347d6bcee88024f5dad886ce
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-10-07 16:58:10 +0200

    Get all currency pair and the volume

--- livecoin/spiders/coin_spider.py
+++ livecoin/spiders/coin_spider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.selector import Selector
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from shutil import which
@@ -8,7 +9,7 @@ from shutil import which
 class CoinSpiderSpider(scrapy.Spider):
     name = 'coin_spider'
     allowed_domains = ['www.livecoin.net/en']
-    start_urls = ['http://www.livecoin.net/en/']
+    start_urls = ['https://www.livecoin.net/en/']
 
     def __init__(self):
         chrome_options = Options()
@@ -20,10 +21,17 @@ class CoinSpiderSpider(scrapy.Spider):
         driver.get("https://www.livecoin.net/en")
 
         rur_tab = driver.find_elements_by_class_name("filterPanelItem___2z5Gb")
-        rur_tab[4].click()
+        rur_tab[2].click()
 
         self.html = driver.page_source
         driver.close()
 
+
     def parse(self, response):
-        pass
+        resp = Selector(text=self.html)
+        table = resp.xpath('//div[@class="ReactVirtualized__Grid__innerScrollContainer"]/div')
+        for currency in table:
+            yield {
+                "currency pair": currency.xpath(".//div[1]/div/text()").get(),
+                "volume(24h)": currency.xpath(".//div[2]/span/text()").get(),
+            }
